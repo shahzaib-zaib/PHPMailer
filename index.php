9a3d5565003170748a478237f8a7285a2170d8dd
Shahzaib <shahzaibux@gmail.com>
2020-08-16 16:02:34 +0500

    Updated Project

--- index.php
+++ index.php
@@ -12,34 +12,54 @@
 
     //Create instance of phpmailer
         $mail = new PHPMailer();
+
     //Set mailer to use smtp
         $mail->isSMTP();
+
     //Define smtp host
         $mail->Host = "smtp.gmail.com";
+
     //Enable smpt authentication
         $mail->SMTPAuth = "true";
+
     //Set type of encryption (SSL/TLS)
         $mail->SMTPSecure = "tls";
+
     //Set port to connect smpt
         $mail->Port = "587";
+
     //Set gmail username
-        $mail->Username = "shahzaibux@gmail.com";
+        $mail->Username = "americalogodesigns@gmail.com";
+
     //Set gmail password
-        $mail->Password = "";
+        $mail->Password = "shahzaib123";
+
     //Set email subject
         $mail->Subject = "Test Email Using PHPMailer";
+
     //Set sender email
-        $mail->setFrom("shahzaibux@gmail.com");
+        $mail->setFrom("americalogodesigns@gmail.com");
+        
+    //Enable HTML
+        $mail->isHTML(true);
+
+    //Attachment
+        $mail->addAttachment('img/attachment.png');
+
     //Email body
-        $mail->Body = "This is plain text email body";
+        $mail->Body = "<h1>This is h1 html heading</h1><br>
+        <p>This is html paragraph</p>";
+
     //Add recipient
-        $mail->addAddress("shahzaibux@gmail.com");
+        $mail->addAddress("americalogodesigns@gmail.com");
+
     //Finally send email
         if ( $mail->Send()) {
             echo "Email Sent..!";
         }else{
             echo "Error..!";
         }
+        
     //Closing smtp connection
         $mail->smtpClose();
 ?>
