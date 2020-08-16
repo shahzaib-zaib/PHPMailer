dc26653bcc4c61b95d1b503a04f336a881949c69
Shahzaib <shahzaibux@gmail.com>
2020-08-16 15:51:33 +0500

    Updated Project

--- index.php
+++ index.php
@@ -23,16 +23,23 @@
     //Set port to connect smpt
         $mail->Port = "587";
     //Set gmail username
-        $mail->Username = "";
+        $mail->Username = "shahzaibux@gmail.com";
     //Set gmail password
         $mail->Password = "";
     //Set email subject
         $mail->Subject = "Test Email Using PHPMailer";
     //Set sender email
-        $mail->setFrom("");
+        $mail->setFrom("shahzaibux@gmail.com");
     //Email body
         $mail->Body = "This is plain text email body";
     //Add recipient
+        $mail->addAddress("shahzaibux@gmail.com");
     //Finally send email
+        if ( $mail->Send()) {
+            echo "Email Sent..!";
+        }else{
+            echo "Error..!";
+        }
     //Closing smtp connection
+        $mail->smtpClose();
 ?>
